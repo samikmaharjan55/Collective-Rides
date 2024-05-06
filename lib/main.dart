a248b34d9acafa77242fc7eadf70a1be1cfe34d5
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-05-06 15:56:27 +0545

    Firebase Setup Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:collective_rides/screens/main_screen.dart';
 import 'package:collective_rides/themeProvider/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
